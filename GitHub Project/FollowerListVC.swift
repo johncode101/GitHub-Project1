a4b3aa516c2074aa0632c43faf30fa2c20333e0b
johncode101 <john.carrera911@gmail.com>
2021-11-17 01:03:59 -0800

    test

--- GitHub Project/FollowerListVC.swift
+++ GitHub Project/FollowerListVC.swift
@@ -16,6 +16,7 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+        
     }
     
 
